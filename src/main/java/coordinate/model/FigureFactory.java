3a2758ccdb92020a7729265604f7bfc95d6ef657
leejinwoo <leejinwoo1126@gmail.com>
2024-01-18 14:30:23 +0900

    feat: FigureFactory (팩토리 메소드 패턴) 기능 추가

---
+++ src/main/java/coordinate/model/FigureFactory.java
@@ -0,0 +1,20 @@
+package coordinate.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public class FigureFactory implements FigureCreator {
+
+    private static final Map<Integer, Function<List<Point>, Figure>> instanceMap = new HashMap<>();
+    static {
+        instanceMap.put(Line.NUM_OF_LINE, Line::new);
+        instanceMap.put(Rectangular.NUM_OF_RECTANGULAR, Rectangular::new);
+    }
+
+    @Override
+    public Figure create(List<Point> points) {
+        return instanceMap.get(points.size()).apply(points);
+    }
+}
