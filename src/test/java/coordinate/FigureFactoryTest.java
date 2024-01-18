41b434cfd43d0e637b30cd775da52099329b71a1
leejinwoo <leejinwoo1126@gmail.com>
2024-01-18 11:11:53 +0900

    exercise remove if

---
+++ src/test/java/coordinate/FigureFactoryTest.java
@@ -0,0 +1,38 @@
+package coordinate;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FigureFactoryTest {
+
+    @Test
+    void line() {
+        List<Point> points = Arrays.asList(Point.of(1, 2), Point.of(2, 3));
+
+        Figure figure = FigureFactory.getInstance(points);
+        assertThat(figure).isInstanceOf(Line.class);
+        assertThat(figure.getName()).isEqualTo("선");
+    }
+
+    @Test
+    void triangle() {
+        List<Point> points = Arrays.asList(Point.of(1, 1), Point.of(4, 1), Point.of(1, 4));
+
+        Figure figure = FigureFactory.getInstance(points);
+        assertThat(figure).isInstanceOf(Triangle.class);
+        assertThat(figure.getName()).isEqualTo("삼각형");
+    }
+
+    @Test
+    void rectangle() {
+        List<Point> points = Arrays.asList(Point.of(1, 1), Point.of(4, 1), Point.of(1, 4), Point.of(4, 4));
+
+        Figure figure = FigureFactory.getInstance(points);
+        assertThat(figure).isInstanceOf(Rectangle.class);
+        assertThat(figure.getName()).isEqualTo("사각형");
+    }
+}
