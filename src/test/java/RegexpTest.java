9ea867e198e2ca2b4746ae8255c24c604407e8be
이진우 <leejinwoo1126@gmail.com>
2024-02-01 14:37:41 +0900

    test: 사각형 관련 정규식 추가, @ParametrizedTest로 변경

--- src/test/java/RegexpTest.java
+++ src/test/java/RegexpTest.java
@@ -1,32 +1,45 @@
 import model.Point;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RegexpTest {
 
-    private String lineInput = "(10,10)-(14,15)";
+    private final String matchRegexp = "\\(\\d{1,2},\\d{1,2}\\)(-\\(\\d{1,2},\\d{1,2}\\)){1,3}";
+    private final String parseRegexp = "\\((\\d{1,2}),(\\d{1,2})\\)";
 
-    @Test
-    void 선입력_매칭() {
-        String regexp = "\\(\\d{1,2},\\d{1,2}\\)-\\(\\d{1,2},\\d{1,2}\\)";
-        Pattern pattern = Pattern.compile(regexp);
-        Matcher matcher = pattern.matcher(lineInput);
+    private static Stream<Arguments> getTestDataProvider() {
+        return Stream.of(
+                Arguments.arguments("(10,10)-(14,15)",
+                        Arrays.asList(new Point(10, 10), new Point(14, 15))),
+                Arguments.arguments("(10,10)-(22,10)-(22,18)-(10,18)",
+                        Arrays.asList(new Point(10, 10), new Point(22, 10), new Point(22, 18), new Point(10, 18)))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getTestDataProvider")
+    void find(String givenInput, List<Point> expected) {
+        Pattern pattern = Pattern.compile(matchRegexp);
+        Matcher matcher = pattern.matcher(givenInput);
 
         assertThat(matcher.find()).isTrue();
     }
 
-    @Test
-    void 선입력_파싱() {
-        String regexp = "\\((\\d{1,2}),(\\d{1,2})\\)";
-        Pattern pattern = Pattern.compile(regexp);
-        Matcher matcher = pattern.matcher(lineInput);
+    @ParameterizedTest
+    @MethodSource("getTestDataProvider")
+    void groupAndParse(String givenInput, List<Point> expected) {
+        Pattern pattern = Pattern.compile(parseRegexp);
+        Matcher matcher = pattern.matcher(givenInput);
 
         List<Point> actual = new ArrayList<>();
         while(matcher.find()) {
@@ -35,8 +48,6 @@ public class RegexpTest {
 
             actual.add(new Point(x, y));
         }
-
-        List<Point> expected = Arrays.asList(new Point(10, 10), new Point(14, 15));
 
         assertThat(actual).isEqualTo(expected);
     }
